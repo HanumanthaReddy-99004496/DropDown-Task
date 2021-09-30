01819586d5d2780c7f53942696fe52302f6049ed
HanumanthaReddy-99004496 <venkata.bodanapu@ltts.com>
2021-09-30 22:09:25 +0530

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { DropdownServiceService } from './dropdown-service.service';
+import { Drop } from './drop';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DropdownTask';
+  constructor(private service:DropdownServiceService){}
+
+  
+
+ 
+
+statesList: Array<any>=[];
+citiesList: Array<any>=[];
+  countrySelected: string ="choose country";
+  stateSelected:string="choose state"
+  countryList:Array<any>=[];
+  ngOnInit(){
+    this.service.fetchData().subscribe(res=>this.countryList=res);
+  }
+countryChange(country:any){
+  console.log(this.countrySelected);
+this.statesList=this.countryList.find((c:any)=>c.name==this.countrySelected).states;
+}
+stateChange(state:any){
+this.citiesList=this.countryList.find((c:any)=>c.name==this.countrySelected).states.find((s:any)=>s.name==this.stateSelected).cities;
+}
 }
